15e80bd2bc2eda189c3d640fabb681985038ea91
Francisco Bertolotto <fbertolotto@fi.uba.ar>
2020-10-27 20:42:49 -0300

    Bug fix

--- client_file_reader.c
+++ client_file_reader.c
@@ -8,9 +8,8 @@ int file_reader_init(file_reader_t* self) {
 }
 
 int file_reader_read(file_reader_t* self, char* buffer, int buf_len) {
-  memset(buffer, 0, buf_len);
   if (feof(self->file)) return 0;
-  return fread(buffer, DATA_LEN, buf_len - 1, self->file);
+  return fread(buffer, DATA_LEN, buf_len, self->file);
 }
 
 int file_reader_destroy(file_reader_t* self) {
--- client_file_reader.h
+++ client_file_reader.h
@@ -13,11 +13,8 @@ Devuelve 0*/
 int file_reader_init(file_reader_t* self);
 
 /* Carga en el buffer lo leído de entrada estándar.
-La cantidad es especificada por el largo del buffer.
-Ademas para evitar problemas, limpia el buffer antes
-de la carga y se reserva la ultima posicion para un 0.
-Devuelve la cantidad de bytes que logro
-leer y 0 si el archivo termino. */
+Devuelve la cantidad de bytes que logro leer y 0 si
+el archivo termino. */
 int file_reader_read(file_reader_t* self, char* buffer, int buf_len);
 
 /* Destruye el lector de archivos. Devuelve 0 */
